e19405726f1f645761bbd87f44b42e6c928ce66d
Zippo <digeronimovalentin@gmail.com>
2023-01-24 01:08:41 -0300

    props synthesized

--- src/helpers/props.js
+++ src/helpers/props.js
@@ -1,30 +1,30 @@
 import { aux } from "./_aux_"
 
-class Input {
-	constructor(pH, mL, id, blank, val, onCh, fix) {
-		this.pH = pH
-		this.mL = mL
-		this.id = id
-		this.blank = blank
-		this.val = val
-		this.onCh = (e) => {
-			if(onCh === 'AZ') aux.reg_Az.test(e.nativeEvent.data) ? fix(e) : null
-			if(onCh === '09') aux.reg_09.test(e.nativeEvent.data) ? fix(e) : null
+export function input(pH, mL, id, blank, val, onCh, fix) {
+	class Input {
+		constructor() {
+			this.pH = pH
+			this.mL = mL
+			this.id = id
+			this.blank = blank
+			this.val = val
+			this.onCh = (e) => {
+				if(onCh === 'AZ') aux.reg_Az.test(e.nativeEvent.data) ? fix(e) : null
+				if(onCh === '09') aux.reg_09.test(e.nativeEvent.data) ? fix(e) : null
+			}
 		}
 	}
-}
 
-class Label {
-	constructor(title, htmlFor) {
-		this.title = title
-		this.htmlFor = htmlFor
-	}
-}
-
-export function input(pH, mL, id, blank, val, onCh, fix) {
-	return new Input(pH, mL, id, blank, val, onCh, fix)
+	return new Input()
 }
 
 export function label(title, htmlFor) {
-	return new Label(title, htmlFor)
+	class Label {
+		constructor() {
+			this.title = title
+			this.htmlFor = htmlFor
+		}
+	}
+
+	return new Label()
 }
